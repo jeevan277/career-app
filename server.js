867c5ca6965bc4aecff7e39dc4eb042583b98dde
jeevan277 <jeevanjosh2003@gmail.com>
2026-03-19 10:41:22 +0530

    fix mongodb timeout

--- server.js
+++ server.js
@@ -107,8 +107,11 @@ app.post('/api/assessment/submit', (req, res) => {
   res.json({ careers: careerScores.slice(0, 5), traits, stream: streams[topCategory] || streams.academic, actionPlan });
 });
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 30000,
+  socketTimeoutMS: 45000,
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
 
 app.listen(process.env.PORT || 5000, () => console.log('Server running on port ' + (process.env.PORT || 5000)));
